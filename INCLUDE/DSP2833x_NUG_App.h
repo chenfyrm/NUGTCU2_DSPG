693937b43327f8bf547c2085abbcdc91707959c6
unknown <700363@nug.com>
2020-03-06 08:00:22 +0800

    1

--- INCLUDE/DSP2833x_NUG_App.h
+++ INCLUDE/DSP2833x_NUG_App.h
@@ -17,27 +17,26 @@
 #define DSP2833x_NUG_APP_H
 //----------------------------------------------------
 extern float32 AI[30];
-extern Uint16 PWM_OS[11];
-
 extern Uint16 DI_OS[1];
-extern Uint16 DO_OS[1];
-extern Uint16 ERR_EXTR[3];
-
 extern Uint16 CFG_IN[4];
 extern Uint16 STA_IN[8];
 extern Uint16 ERR_DSP[3];
+extern Uint16 ERR_EXTR[3];
+
+extern Uint16 PWM_OS[11];
+extern Uint16 DO_OS[1];
 extern Uint16 CFG_OUT[3];
 extern Uint16 STA_OUT[4];
-extern Uint16 CUST_MCU_IM[30];
-extern Uint16 CUST_MCU_1ms[10];
-extern Uint16 CUST_MCU_2ms[20];
-//extern Uint16 CUST_MCU_16ms[20];
-//extern Uint16 CUST_MCU_64ms[20];
-//extern Uint16 CUST_DSP_IM[30];
-extern Uint16 CUST_DSP_1ms[10];
-extern Uint16 CUST_DSP_2ms[20];
-//extern Uint16 CUST_DSP_16ms[20];
-//extern Uint16 CUST_DSP_64ms[20];
+
+extern Uint16 CUST_MCU_PAR[100];
+extern Uint16 CUST_MCU_1ms[20];
+extern Uint16 CUST_MCU_2ms[40];
+extern Uint16 CUST_MCU_16ms[40];
+extern Uint16 CUST_MCU_64ms[40];
+extern Uint16 CUST_DSP_1ms[20];
+extern Uint16 CUST_DSP_2ms[40];
+extern Uint16 CUST_DSP_16ms[40];
+extern Uint16 CUST_DSP_64ms[40];
 
 
 #ifdef __cplusplus
